2401555b095083ff2307de645cafdebfdb29a627
Basvi <>
2025-04-03 15:47:50 +1000

    css

--- src/BookingForm.tsx
+++ src/BookingForm.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Form, Input, Select, DatePicker, InputNumber, Checkbox, Button } from 'antd';
+import './App.css';
 
 const { Option } = Select;
 const { TextArea } = Input;
@@ -12,66 +13,68 @@ const BookingForm: React.FC = () => {
   };
 
   return (
-    <Form
-      form={form}
-      name="booking"
-      layout="vertical"
-      onFinish={onFinish}
-      initialValues={{ tripType: 'Пляжный отдых' }}
-    >
-      <h1>Забронируйте свое путешествие!</h1>
-      <Form.Item
-        label="Ваше имя:"
-        name="name"
-        rules={[{ required: true, message: 'Пожалуйста, введите ваше имя!' }]}
+    <div className="booking-form">
+      <Form
+        form={form}
+        name="booking"
+        layout="vertical"
+        onFinish={onFinish}
+        initialValues={{ tripType: 'Пляжный отдых' }}
       >
-        <Input />
-      </Form.Item>
-      <Form.Item
-        label="Ваш email:"
-        name="email"
-        rules={[{ required: true, message: 'Пожалуйста, введите ваш email!' }]}
-      >
-        <Input type="email" />
-      </Form.Item>
-      <Form.Item label="Тип путешествия:" name="tripType">
-        <Select>
-          <Option value="Экскурсионный тур">Экскурсионный тур</Option>
-          <Option value="Пляжный отдых">Пляжный отдых</Option>
-          <Option value="Приключения">Приключения</Option>
-          <Option value="Круиз">Круиз</Option>
-        </Select>
-      </Form.Item>
-      <Form.Item
-        label="Количество путешественников:"
-        name="travelers"
-      >
-        <InputNumber min={1} max={10} />
-      </Form.Item>
-      <Form.Item
-        label="Дата начала путешествия:"
-        name="startDate"
-      >
-        <DatePicker />
-      </Form.Item>
-      <Form.Item label="Ваши пожелания:" name="preferences">
-        <TextArea rows={4} />
-      </Form.Item>
-      <Form.Item
-        name="agreement"
-        valuePropName="checked"
-        rules={[{ validator: (_, value) => value ? Promise.resolve() : Promise.reject(new Error('Согласие обязательно!')) }]}
-      >
-        <Checkbox>
-          Я согласен на обработку моих персональных данных.
-        </Checkbox>
-      </Form.Item>
-      <Form.Item>
-        <Button type="primary" htmlType="submit">
-          Забронировать
-        </Button>
-      </Form.Item>
-    </Form>
+        <h1>Забронируйте свое путешествие!</h1>
+        <Form.Item
+          label="Ваше имя:"
+          name="name"
+          rules={[{ required: true, message: 'Пожалуйста, введите ваше имя!' }]}
+        >
+          <Input />
+        </Form.Item>
+        <Form.Item
+          label="Ваш email:"
+          name="email"
+          rules={[{ required: true, message: 'Пожалуйста, введите ваш email!' }]}
+        >
+          <Input type="email" />
+        </Form.Item>
+        <Form.Item label="Тип путешествия:" name="tripType">
+          <Select>
+            <Option value="Экскурсионный тур">Экскурсионный тур</Option>
+            <Option value="Пляжный отдых">Пляжный отдых</Option>
+            <Option value="Приключения">Приключения</Option>
+            <Option value="Круиз">Круиз</Option>
+          </Select>
+        </Form.Item>
+        <Form.Item
+          label="Количество путешественников:"
+          name="travelers"
+        >
+          <InputNumber min={1} max={10} />
+        </Form.Item>
+        <Form.Item
+          label="Дата начала путешествия:"
+          name="startDate"
+        >
+          <DatePicker />
+        </Form.Item>
+        <Form.Item label="Ваши пожелания:" name="preferences">
+          <TextArea rows={4} />
+        </Form.Item>
+        <Form.Item
+          name="agreement"
+          valuePropName="checked"
+          rules={[{ validator: (_, value) => value ? Promise.resolve() : Promise.reject(new Error('Согласие обязательно!')) }]}
+        >
+          <Checkbox>
+            Я согласен на обработку моих персональных данных.
+          </Checkbox>
+        </Form.Item>
+        <Form.Item>
+          <Button type="primary" htmlType="submit">
+            Забронировать
+          </Button>
+        </Form.Item>
+      </Form>
+    </div>
   );
 };
 
--- src/DestinationPage.tsx
+++ src/DestinationPage.tsx
@@ -1,9 +1,20 @@
 import React from 'react';
+import './App.css';
+
+const imageStyle: React.CSSProperties = {
+  height: '30%',
+  width: '30%'
+}
 
 const DestinationPage: React.FC = () => {
   return (
-    <div>
+    <div className="destination-page">
       <h1>Мальдивы - рай на Земле!</h1>
+      <img
+        src={require('./assets/maldives.jpg')}
+        style={imageStyle}
+        alt="Мальдивы"
+      />
       <p>
         Мальдивы - это тропический рай, известный своими белоснежными пляжами, кристально чистой водой и роскошными курортами.
         Здесь можно заняться дайвингом, снорклингом, виндсерфингом или просто расслабиться на пляже, наслаждаясь солнцем.
